3b8c2cdf6bef74bbc3efd797d74bbd5d1e7595e8
NoThrottle <ntyt.em@gmail.com>
2022-01-21 06:42:44 +0800

    Add project files.

---
+++ filename to list/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace filename_to_list
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inbrowse = new System.Windows.Forms.Button();
+            this.filepath = new System.Windows.Forms.TextBox();
+            this.outbrowse = new System.Windows.Forms.Button();
+            this.outpath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.logs = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inbrowse
+            // 
+            this.inbrowse.Location = new System.Drawing.Point(248, 28);
+            this.inbrowse.Name = "inbrowse";
+            this.inbrowse.Size = new System.Drawing.Size(75, 23);
+            this.inbrowse.TabIndex = 0;
+            this.inbrowse.Text = "Browse";
+            this.inbrowse.UseVisualStyleBackColor = true;
+            this.inbrowse.Click += new System.EventHandler(this.inbrowse_Click);
+            // 
+            // filepath
+            // 
+            this.filepath.Location = new System.Drawing.Point(13, 28);
+            this.filepath.Name = "filepath";
+            this.filepath.Size = new System.Drawing.Size(229, 20);
+            this.filepath.TabIndex = 1;
+            // 
+            // outbrowse
+            // 
+            this.outbrowse.Location = new System.Drawing.Point(248, 74);
+            this.outbrowse.Name = "outbrowse";
+            this.outbrowse.Size = new System.Drawing.Size(75, 23);
+            this.outbrowse.TabIndex = 2;
+            this.outbrowse.Text = "Browse";
+            this.outbrowse.UseVisualStyleBackColor = true;
+            this.outbrowse.Click += new System.EventHandler(this.outbrowse_Click);
+            // 
+            // outpath
+            // 
+            this.outpath.Location = new System.Drawing.Point(13, 76);
+            this.outpath.Name = "outpath";
+            this.outpath.Size = new System.Drawing.Size(229, 20);
+            this.outpath.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "List and Logs output.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Path of .wav files.";
+            // 
+            // logs
+            // 
+            this.logs.Location = new System.Drawing.Point(13, 145);
+            this.logs.Multiline = true;
+            this.logs.Name = "logs";
+            this.logs.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logs.Size = new System.Drawing.Size(310, 253);
+            this.logs.TabIndex = 6;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 116);
+            this.progressBar1.MarqueeAnimationSpeed = 0;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(230, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 7;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(248, 116);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 8;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 410);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.logs);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outpath);
+            this.Controls.Add(this.outbrowse);
+            this.Controls.Add(this.filepath);
+            this.Controls.Add(this.inbrowse);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Filename to List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button inbrowse;
+        private System.Windows.Forms.TextBox filepath;
+        private System.Windows.Forms.Button outbrowse;
+        private System.Windows.Forms.TextBox outpath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.TextBox logs;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button start;
+    }
+}
+
---
+++ filename to list/Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Shell;
+using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
+
+namespace filename_to_list
+{
+    public partial class Form1 : Form
+    {
+        #region Private Variables
+        string inpath = null;
+        string outend = null;
+        List<string> filestotrans = new List<string>();
+        List<string> pathoffilestotrans = new List<string>();
+        List<string> rawpathfilestotrans = new List<string>();
+        List<string> voiceline = new List<string>();
+
+        static string logline = null;
+
+        static int maxval = 1;
+        static int curval = 0;
+
+        TextWriter _writer = null;
+        #endregion
+
+        //Edit These
+        string extension = "wav"; //"wav" default
+
+        private void listadd(int i, string line)
+        {
+            voiceline.Add("wavs/" + i + ".wav| " + line);
+        }
+
+        //-----------
+        //Don't touch anything below unless you're modifying 'the software'.
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ListOfFiles()
+        {
+            FileInfo[] files = new DirectoryInfo(inpath).GetFiles().AsEnumerable().OrderBy(item => item.FullName, new NaturalSortComparer<string>()).ToArray();
+            progressBar1.Maximum = files.Length;
+            progressBar1.Value = 0;
+
+            int i = 0;
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine(file.Directory.ToString());
+
+                string end = file.FullName.Substring(file.FullName.Length - extension.Length, extension.Length);
+                curval = i;
+                progressBar1.Value = curval;
+
+                if (end == extension)
+                {
+                    filestotrans.Add(ShellFile.FromFilePath(Path.Combine(file.Directory.ToString(), file.FullName)).Properties.System.Title.Value);
+                    rawpathfilestotrans.Add(file.Directory.ToString());
+                    pathoffilestotrans.Add(Path.Combine(file.Directory.ToString(), file.FullName));
+
+                    logline = ("Added:" + file.FullName + " From: " + file.Directory.ToString());
+                    i++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+
+        private void processfiles()
+        {
+            Console.WriteLine("Processing Files");
+
+            progressBar1.Maximum = filestotrans.Count;
+            curval = 0;
+            int i = 0;
+
+            foreach(string filesto in filestotrans)
+            {
+                curval++;
+                Console.WriteLine("Processing: " + pathoffilestotrans[i]);
+                Console.WriteLine("To: " + Path.Combine(rawpathfilestotrans[i], i + "." + extension));
+                System.IO.File.Move(pathoffilestotrans[i], Path.Combine(rawpathfilestotrans[i], i + "." + extension));
+                listadd(i, filestotrans[i]);
+                Console.WriteLine("Added to list: " + voiceline[i]);
+                progressBar1.Value = curval;
+                i++;
+            }
+        }
+
+        private void writelist()
+        {
+            Console.WriteLine("Writing to File");
+            StreamWriter sw = new StreamWriter(Path.Combine(outend, "list.txt"));
+
+            progressBar1.Maximum = voiceline.Count;
+            curval = 0;
+
+
+            foreach (var voice in voiceline)
+            {
+                
+                curval++;
+                progressBar1.Value = curval;
+                Console.WriteLine(voice);
+                sw.WriteLine(voice);
+            }
+
+            sw.Close();
+        }
+
+        private void writeLogs()
+        {
+            Console.WriteLine("Writing Logs");
+            StreamWriter sw = new StreamWriter(Path.Combine(outend, "logs." + DateTime.Now.ToFileTime() + ".txt"));
+            progressBar1.Maximum = logs.Text.Split(
+                        new string[] { Environment.NewLine },
+                        StringSplitOptions.None).Length;
+            curval = 0;
+
+
+
+            foreach (var voice in logs.Text.Split(
+                        new string[] { Environment.NewLine },
+                        StringSplitOptions.None))
+            {
+
+                curval++;
+                progressBar1.Value = curval;
+            }
+
+            sw.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _writer = new TextBoxStreamWriter(logs);
+            // Redirect the out Console stream
+            Console.SetOut(_writer);
+        }
+
+        private void inbrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog give = new FolderBrowserDialog();
+            give.ShowDialog();
+
+            filepath.Text = give.SelectedPath;
+            outpath.Text = filepath.Text;
+
+        }
+
+        private void outbrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog give = new FolderBrowserDialog();
+            give.ShowDialog();
+
+            outpath.Text = give.SelectedPath;
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(outpath.Text) && string.IsNullOrEmpty(filepath.Text)) 
+            {
+                MessageBox.Show("At least one of the paths are empty", "Error", MessageBoxButtons.OK);
+
+            }
+            else
+            {
+
+                filestotrans = null;
+                rawpathfilestotrans = null;
+                voiceline = null;
+                pathoffilestotrans = null;
+
+                filestotrans = new List<string>();
+                rawpathfilestotrans = new List<string>();
+                voiceline = new List<string>();
+                pathoffilestotrans = new List<string>();
+
+                Console.WriteLine("Initiated");
+
+                inpath = filepath.Text;
+                outend = outpath.Text;
+
+                ListOfFiles();
+                processfiles();
+                writelist();
+                writeLogs();
+            }
+
+
+        }
+    }
+
+    public class TextBoxStreamWriter : TextWriter
+    {
+        TextBox _output = null;
+
+        public TextBoxStreamWriter(TextBox output)
+        {
+            _output = output;
+        }
+
+        public override void Write(char value)
+        {
+            base.Write(value);
+
+            if (value.ToString() == "\n")
+            {
+                _output.AppendText("\r\n" + DateTime.Now + ": "); // When character data is written, append it to the text box.
+
+            }
+            else
+            {
+                _output.AppendText(value.ToString());
+            }
+
+        }
+
+        public override Encoding Encoding
+        {
+            get { return System.Text.Encoding.UTF8; }
+        }
+    }
+
+    public class NaturalSortComparer<T> : IComparer<string>, IDisposable
+    {
+        private bool isAscending;
+
+        public NaturalSortComparer(bool inAscendingOrder = true)
+        {
+            this.isAscending = inAscendingOrder;
+        }
+
+        #region IComparer<string> Members
+
+        public int Compare(string x, string y)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IComparer<string> Members
+
+        int IComparer<string>.Compare(string x, string y)
+        {
+            if (x == y)
+                return 0;
+
+            string[] x1, y1;
+
+            if (!table.TryGetValue(x, out x1))
+            {
+                x1 = Regex.Split(x.Replace(" ", ""), "([0-9]+)");
+                table.Add(x, x1);
+            }
+
+            if (!table.TryGetValue(y, out y1))
+            {
+                y1 = Regex.Split(y.Replace(" ", ""), "([0-9]+)");
+                table.Add(y, y1);
+            }
+
+            int returnVal;
+
+            for (int i = 0; i < x1.Length && i < y1.Length; i++)
+            {
+                if (x1[i] != y1[i])
+                {
+                    returnVal = PartCompare(x1[i], y1[i]);
+                    return isAscending ? returnVal : -returnVal;
+                }
+            }
+
+            if (y1.Length > x1.Length)
+            {
+                returnVal = 1;
+            }
+            else if (x1.Length > y1.Length)
+            {
+                returnVal = -1;
+            }
+            else
+            {
+                returnVal = 0;
+            }
+
+            return isAscending ? returnVal : -returnVal;
+        }
+
+        private static int PartCompare(string left, string right)
+        {
+            int x, y;
+            if (!int.TryParse(left, out x))
+                return left.CompareTo(right);
+
+            if (!int.TryParse(right, out y))
+                return left.CompareTo(right);
+
+            return x.CompareTo(y);
+        }
+
+        #endregion
+
+        private Dictionary<string, string[]> table = new Dictionary<string, string[]>();
+
+        public void Dispose()
+        {
+            table.Clear();
+            table = null;
+        }
+    }
+}
